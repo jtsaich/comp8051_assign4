4b42ddcdaeb8d6c2e423514d7116a4f7918add76
Jack Tsai <jtsai0606@gmail.com>
2016-03-27 12:55:09 -0700

    moved Pong to subdirectory, create Breakout as another project

---
+++ Breakout/Breakout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Breakout
+//
+//  Created by Jack Tsai on 3/27/16.
+//  Copyright © 2016 BCIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Breakout/Breakout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Breakout
+//
+//  Created by Jack Tsai on 3/27/16.
+//  Copyright © 2016 BCIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
